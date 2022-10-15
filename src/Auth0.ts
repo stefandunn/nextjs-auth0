7203507817ce901034f61009f25aee9079b715e9
Stefan Dunn <stefannicholadsdunn@googlemail.com>
2022-10-15 16:49:46 +0100

    Removed unused files

--- src/Auth0.ts
+++
@@ -1,8 +0,0 @@
-import { Auth0Client } from "@auth0/auth0-spa-js";
-
-export const auth0 = new Auth0Client({
-  domain: process.env.NEXT_PUBLIC_AUTH0_DOMAIN,
-  client_id: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
-  redirect_uri:
-    typeof window !== "undefined" ? window.location.href : undefined,
-});
